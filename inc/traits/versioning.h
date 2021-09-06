c56b247a27a7936263cace3843b31540ec5f44fa
joeri-hu <joeri.j.kok@student.hu.nl>
2021-09-06 02:47:43 +0200

    simplify versioning traits

--- inc/traits/versioning.h
+++ inc/traits/versioning.h
@@ -5,31 +5,26 @@
 
 inline namespace gl {
 namespace ts {
-namespace version {
-namespace internal {
 
-template<long N>
-struct id {
-    static constexpr auto value = N;
-
-    using is_current = std::integral_constant<bool,
-        N != 0 and N == __cplusplus + 0>;
+enum class version {
+    unknown,
+    cpp11 = 201103L,
+    cpp14 = 201402L,
+    cpp17 = 201703L,
+    cpp20 = 202002L,
+    current = __cplusplus + unknown
 };
 
-} // namespace internal
+template<version V>
+using std_matches = std::integral_constant<bool,
+    V != version::unknown and V == version::current>;
 
-struct cpp11 : internal::id<201103L> {};
-struct cpp14 : internal::id<201402L> {};
-struct cpp17 : internal::id<201703L> {};
-struct cpp20 : internal::id<202002L> {};
-
-} // namespace version
 inline namespace helpers {
 
-using is_cpp11_compliant = version::cpp11::is_current;
-using is_cpp14_compliant = version::cpp14::is_current;
-using is_cpp17_compliant = version::cpp17::is_current;
-using is_cpp20_compliant = version::cpp20::is_current;
+using is_cpp11_compliant = std_matches<version::cpp11>;
+using is_cpp14_compliant = std_matches<version::cpp14>;
+using is_cpp17_compliant = std_matches<version::cpp17>;
+using is_cpp20_compliant = std_matches<version::cpp20>;
 
 constexpr auto is_cpp11_compliant_v() noexcept -> bool
 { return is_cpp11_compliant{}; }

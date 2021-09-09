dca2676609295936a4ecddb41562e069903f2c22
joeri-hu <joeri.j.kok@student.hu.nl>
2021-09-09 15:03:59 +0200

    revert versatility changes constraints

--- inc/traits/constraints.h
+++ inc/traits/constraints.h
@@ -6,22 +6,22 @@
 namespace ts {
 
 template<typename T>
-struct require : std::enable_if_t<T{}> {};
+using require = std::enable_if_t<T{}>;
 
 template<typename T>
-struct require_not : require<std::negation<T>> {};
+using require_not = require<std::negation<T>>;
 
 template<typename... Ts>
-struct require_all : require<std::conjunction<Ts...>> {};
+using require_all = require<std::conjunction<Ts...>>;
 
 template<typename T1, typename T2>
-struct require_both : require_all<T1, T2> {};
+using require_both = require_all<T1, T2>;
 
 template<typename... Ts>
-struct require_either : require<std::disjunction<Ts...>> {};
+using require_either = require<std::disjunction<Ts...>>;
 
 template<typename... Ts>
-struct require_neither : require_not<std::disjunction<Ts...>> {};
+using require_neither = require_not<std::disjunction<Ts...>>;
 
 } // namespace ts
 

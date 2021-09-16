3b9a6cf1494359b3dadabc1e31c3d0b82f91ce2d
joeri-hu <joeri.j.kok@student.hu.nl>
2021-09-16 21:27:33 +0200

    reorder header inclusions

--- inc/traits.h
+++ inc/traits.h
@@ -2,8 +2,8 @@
 #define GL_TRAITS_H
 
 #include <traits/categories.h>
+#include <traits/characteristics.h>
 #include <traits/constraints.h>
-#include <traits/properties.h>
 #include <traits/transformations.h>
 #include <traits/versioning.h>
 
